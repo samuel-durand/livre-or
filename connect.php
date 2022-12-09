18046e61db98c51edd474ec207aa2ace3d131433
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2022-12-09 11:49:32 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,8 @@
 <?php
 
 session_start();
-$mysqli = new mysqli("localhost:3306","root","","livreor");
+//$mysqli = new mysqli("localhost:3306","root","","livreor");
+$mysqli = new mysqli("localhost:3306", "samuel2", "T9#g77h7t", "samuel-durand_livreor");
 
 
 
